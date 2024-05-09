64d17e3c3fb105584b077c0dcd5ba3245fe1e6cd
rohitkr0017 <rohitkr0017@gmail.com>
2024-05-09 11:22:24 +0530

    task1 456 completed

--- test1.java
+++ test1.java
@@ -3,6 +3,11 @@ public class Test1.java
 	public void m2() {
 		System.out.println("Welcome");
 	}
+
+	public void m3() {
+		System.out.println("Task 456 completed");
+	}
+
 	public static void main(String args[])
 	{
 		System.out.println("Hello");
